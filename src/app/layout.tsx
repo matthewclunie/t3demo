04a30e629dfe4f4c0f5142d9f167e0422672751d
matthewclunie <matthew.clunie@hunterbusinessschool.edu>
2023-11-27 18:38:35 -0500

    added basePath url

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter } from "next/font/google";
 import { cookies } from "next/headers";
 
 import { TRPCReactProvider } from "~/trpc/react";
+import { SessionProvider } from "next-auth/react";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -24,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`font-sans ${inter.variable}`}>
-        <TRPCReactProvider cookies={cookies().toString()}>
+        <TRPCReactProvider cookies={cookies().toString()} basePath="/api/auth">
           {children}
         </TRPCReactProvider>
       </body>
